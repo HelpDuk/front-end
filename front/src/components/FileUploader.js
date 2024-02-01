fd53f925c5be218f1a61cb795519c2dc02a4ff1e
hyom1n <hyomin0806@gmaile.com>
2024-02-01 14:21:03 +0900

    FileUpload.js 파일첨부 수정

--- front/src/components/FileUploader.js
+++ front/src/components/FileUploader.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import "../styles/RequestContent.css";
-import upload from "../assets/image/upload.png";
+import camera from "../assets/image/camera.png";
 import "./LocationTimeSelector.js";
 import LocationTimeSelector from './LocationTimeSelector.js';
 
 function FileUploader() {
     // 파일 상태
     const [file, setFile] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
     // 파일 선택 핸들러
     const handleFileChange = (e) => {
         const selectedFile = e.target.files[0];
-        setFile(selectedFile);
+        // 이미지 파일인지 확인
+        if (selectedFile && selectedFile.type.includes('image')) {
+            setFile(selectedFile);
+            setErrorMessage('');
+        } else {
+            setFile(null);
+            setErrorMessage('이미지 파일만 선택해주세요.');
+        }
     };
 
     return (
@@ -20,10 +28,11 @@ function FileUploader() {
             <br />
             <div className='file-upload'>
                 <div className="upload-wrapper">
-                    <img src={upload} alt="upload" className="upload-image" />
-                    <input className="submit" type="file" onChange={handleFileChange} />
+                    <img src={camera} alt="camera" className="camera" />
+                    <input className="submit" type="file" accept="image/png, image/jpeg, image/gif" onChange={handleFileChange} />
                 </div>
             </div>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         </div>
     );
 }
